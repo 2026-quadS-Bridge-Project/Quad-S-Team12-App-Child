0576f30a0716a4326fe30be1a2b1648d3439fa75
yeongJD <pyeon136@gmail.com>
2026-06-11 08:25:50 +0900

    style: align child notification card radius

--- lib/features/notifications/presentation/widgets/notification_card.dart
+++ lib/features/notifications/presentation/widgets/notification_card.dart
@@ -110,7 +110,7 @@ class _NotificationCardState extends State<NotificationCard>
                   width: _maxSlide + 22,
                   decoration: BoxDecoration(
                     color: AppColors.destructiveSubtle,
-                    borderRadius: BorderRadius.circular(14.385),
+                    borderRadius: BorderRadius.circular(AppTokens.dialogRadius),
                   ),
                   child: Align(
                     alignment: Alignment.centerRight,
